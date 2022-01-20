7fb25986eee1b93796690974b1c850d017b680a0
Chris <chrsv00ng6@gmail.com>
2022-01-19 18:03:52 -0800

    flywheel code

--- Roadkill/test/src/main/java/frc/robot/FlywheelContainer.java
+++ Roadkill/test/src/main/java/frc/robot/FlywheelContainer.java
@@ -11,13 +11,15 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 /** Add your docs here. */
-public class CaitpultContainer {
+public class FlywheelContainer {
     XboxController mXbox;
     private Motor motor;
+    private Motor motor2;
     private final SpinMotor m_SpinMotor = new SpinMotor(motor, 0.5);
 
-    public CaitpultContainer() {
+    public FlywheelContainer() {
         motor = new Motor(7);
+        motor2 = new Motor(6);
         mXbox = new XboxController(0);
         configureButtonBindings();
         
@@ -26,8 +28,8 @@ public class CaitpultContainer {
     private void configureButtonBindings() {
         JoystickButton buttonA = new JoystickButton(mXbox, XboxController.Button.kA.value);
 
-        buttonA.whenPressed(new SpinMotor(motor, 0.7).withTimeout(0.1));
-
+        buttonA.whenPressed(new SpinMotor(motor, 1));
+        buttonA.whenPressed(new SpinMotor(motor2, -1));
         
         //JoystickButton buttonA = new JoystickButton(xbox, XboxController.Button.kA.value);
 
